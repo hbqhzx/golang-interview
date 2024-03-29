e5e5018fb5d7d5131aeae6532cc9b46d189f7850
zhaoxiao <zhaoxiao.248@bytedance.com>
2024-03-29 16:05:39 +0800

    feat: 多协程打印

--- 算法与数据结构/channel/多个协程交替打印1-1000.go
+++ 算法与数据结构/channel/多个协程交替打印1-1000.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func printNum() {
+	wg := sync.WaitGroup{}
+	ch1, ch2 := make(chan struct{}), make(chan struct{})
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for i := 1; i <= 10; i = i + 2 {
+			<-ch1
+			fmt.Println("A: ", i)
+			ch2 <- struct{}{}
+		}
+		<-ch1
+	}()
+	go func() {
+		defer wg.Done()
+		for i := 2; i <= 10; i = i + 2 {
+			<-ch2
+			fmt.Println("B: ", i)
+			ch1 <- struct{}{}
+		}
+	}()
+	ch1 <- struct{}{}
+	wg.Wait()
+}
